06a2589473c05172fbc97bfd8085535b5f64229b
Salman1717 <salmanmhaskar17@gmail.com>
2026-01-22 23:20:59 +0530

    Initial Commit

---
+++ AppForgeAI/AppForgeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppForgeAIApp.swift
+//  AppForgeAI
+//
+//  Created by Salman Mhaskar on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AppForgeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppForgeAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppForgeAI
+//
+//  Created by Salman Mhaskar on 22/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
